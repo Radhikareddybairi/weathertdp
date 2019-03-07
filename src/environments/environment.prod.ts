a38afbab68b65ef1f8076403eb9c4457246025ae
Radhikabairi <radhikareddy300@gmail.com>
2019-03-07 12:23:51 +0530

    version of web

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebaseconfig: {
+    
+      apiKey: "AIzaSyBXjCP4DRfaad__Z7HeyfSIsIOm8_LhoGo",
+      authDomain: "weathertdp.firebaseapp.com",
+      databaseURL: "https://weathertdp.firebaseio.com",
+      projectId: "weathertdp",
+      storageBucket: "weathertdp.appspot.com",
+      messagingSenderId: "379333860430"
+    
+  }
+};
